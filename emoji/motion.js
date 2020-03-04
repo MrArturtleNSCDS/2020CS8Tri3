5f46357b5380a14593a29bc462754bb1621590b7
MrArturtleNSCDS <agarcia@nscds.org>
2020-03-04 21:18:37 +0000

    Eyebrowmovement

--- emoji/motion.js
+++ emoji/motion.js
@@ -17,3 +17,7 @@ TweenMax.to("#rightEye .pupil", 3.1 ,{
     },
     ease:Power0.easeNone,repeat:-1
 });
+
+TweenMax.to("#rightEye .eyebrow", 2, {rotation: -40, x:-10, y:4, repeat:-1, yoyo:true});
+TweenMax.to("#leftEye .eyebrow", 2, {rotation: 40, x:10, y:4, repeat:-1, yoyo:true});
+TweenMax.to("#face", 2, {backgroundColor: "red", repeat:-1, yoyo:true});
