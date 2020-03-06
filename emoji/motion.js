19d13622343e727f33881872b2a6884d19751d59
MrArturtleNSCDS <agarcia@nscds.org>
2020-03-06 19:08:09 +0000

    Corrected motion

--- emoji/motion.js
+++ emoji/motion.js
@@ -2,7 +2,7 @@ var diameter = 20;
 var clockWise = [{x:0,y:0},{x:diameter/2,y:diameter/2},{x:0,y:diameter},{x:-diameter/2,y:diameter/2},{x:0,y:0}];
 var counterClockWise = [{x:0,y:0},{x:-diameter/2,y:diameter/2},{x:0,y:diameter},{x:diameter/2,y:diameter/2},{x:0,y:0}]
 
-TweenMax.to("#leftEye .pupil", 3 ,{
+TweenMax.to("#leftEye .pupil", .3 ,{
     bezier:{
         curviness:1.5,
         values: clockWise
@@ -20,4 +20,4 @@ TweenMax.to("#rightEye .pupil", 3.1 ,{
 
 TweenMax.to("#rightEye .eyebrow", 2, {rotation: -40, x:-10, y:4, repeat:-1, yoyo:true});
 TweenMax.to("#leftEye .eyebrow", 2, {rotation: 40, x:10, y:4, repeat:-1, yoyo:true});
-TweenMax.to("#face", 2, {backgroundColor: "red", repeat:-1, yoyo:true});
+TweenMax.to("#face", .2, {backgroundColor: "red", repeat:-1, yoyo:true});
